551d44f8e9182b348d64efe0f934d7bcabf64a00
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2022-11-27 21:37:14 +0100

    Update PWA-related Next.js configuration
    Two changes here:
    - fix GenerateSW warning that occurred on each rebuild in webpack --watch mode
    - give a clearer name to the service-worker.js file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
+  sw: 'service-worker.js',
+  disable: process.env.NODE_ENV === 'development'
 })
 
 module.exports = withPWA({
