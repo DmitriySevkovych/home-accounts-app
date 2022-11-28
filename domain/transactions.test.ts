a55feb1ae74b0fcba2a4367ec2c86dcd01d03eba
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2022-11-28 17:11:51 +0100

    Add first dummy model and first dummy test

---
+++ domain/transactions.test.ts
@@ -0,0 +1,13 @@
+import { Transaction } from "./transactions"
+
+
+describe('Transactions tests', () => {
+
+    it('should work and currently do nothing', () => {
+        // Arrange
+        const transaction = new Transaction()
+        // Act
+        // Assert
+        expect(transaction).toBeDefined()
+    })
+})
