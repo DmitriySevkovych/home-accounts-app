dfb1fac0a76cc5307fa267fee4ef132672ddc68f
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2022-11-28 20:26:58 +0100

    Update dummy route

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,8 +1,10 @@
+import { Transaction } from '../../domain/transactions'
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from 'next'
-
 const hello = (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ name: 'John Doe' })
+  const transaction = new Transaction()
+  res.status(200).json(transaction)  
 }
 
 export default hello
