5aa4f821ee1e36566ce6a105ec4dea3c941edd34
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2023-03-20 17:21:07 +0100

    WIP: Introduce domain model

--- domain/transactions.ts
+++ domain/transactions.ts
@@ -1,7 +1,52 @@
-export class Transaction {
-    details: TransactionDetails = new TransactionDetails()
+type BankAccount = string
+
+type Document = {
+    _id: number
+    date: Date
+    name: string
+    mimetype: string
+    buffer: BinaryData // TODO is this correct? is this necessary?
+    description: string
 }
 
-export class TransactionDetails {
+//
 
+type Expense = {
+    _id: number
+    type: string
+    origin: string
+    description: string
+    transaction: Transaction
 }
+
+export type HomeExpense = Expense
+
+export type WorkExpense = Expense & {
+    country: string
+    vat: number
+}
+
+export type InvestmentExpense = Expense & {
+    investmentObject: string
+}
+
+//
+
+export type Transaction = {
+    _id: number
+    date: Date
+    amount: number
+    currency: string
+    exchange_rate: number
+    source_bank_account: BankAccount
+    target_bank_account: BankAccount
+    agent: string
+    payment_method: string
+    // tax_relevant: boolean
+    tax_category?: string
+    comment?: string
+    receipt_id?: number
+}
+
+
+export type TransactionReceipt = Document
