9d32a13cd3c8a7f162b4747fd4b2efb01e2a0fb8
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2023-03-28 06:52:23 +0200

    Fix: replace incorrect TypeScript type usage

--- domain/transactions.test.ts
+++ domain/transactions.test.ts
@@ -5,7 +5,17 @@ describe('Transactions tests', () => {
 
     it('should work and currently do nothing', () => {
         // Arrange
-        const transaction = new Transaction()
+        const transaction: Transaction = {
+            _id: 1,
+            date: new Date(),
+            amount: -1,
+            currency: 'EUR',
+            exchange_rate: 1,
+            source_bank_account: 'DummySourceBank',
+            target_bank_account: 'DummyTargetBank',
+            agent: 'DummyAgent',
+            payment_method: 'TRANSFER'
+        }
         // Act
         // Assert
         expect(transaction).toBeDefined()
