ce98338c1dee27618b71060df924b6ec2de5c7be
ivannaSevkovychItk <ivanna.sevkovych@itk-engineering.de>
2023-04-03 19:05:16 +0200

    Feat: add exchangeRate to transaction

--- domain/transactions.test.ts
+++ domain/transactions.test.ts
@@ -1,19 +1,33 @@
-import {Transaction} from './transactions'
-
+import { Transaction } from './transactions'
 
 describe('Transactions tests', () => {
-
     it.each`
         amount | expectedTransactionType
-        ${20} | ${'income'}
-        ${-5} | ${'expense'}
-    `('should return $expectedTransactionType as transaction type when amount is $amount', ({amount, expectedTransactionType}) => {
-        // Arrange
-        const transaction = new Transaction(amount)
-        // Act
-        const transactionType = transaction.type()
-        // Assert
-        expect(transactionType).toBe(expectedTransactionType)
-    })
+        ${20}  | ${'income'}
+        ${-5}  | ${'expense'}
+    `(
+        'should return $expectedTransactionType as transaction type when amount is $amount',
+        ({ amount, expectedTransactionType }) => {
+            // Arrange
+            const transaction = new Transaction({
+                amount,
+            })
+            // Act
+            const transactionType = transaction.type()
+            // Assert
+            expect(transactionType).toBe(expectedTransactionType)
+        }
+    )
 
+    it('should return right equivalent in EUR', () => {
+        // Arrange
+        const transaction = new Transaction({
+            amount: 10,
+            exchangeRate: 0.8,
+        })
+        // Act
+        const transactionEurEquivalent = transaction.eurEquivalent()
+        // Assert
+        expect(transactionEurEquivalent).toBe(8)
+    })
 })
--- domain/transactions.ts
+++ domain/transactions.ts
@@ -1,13 +1,27 @@
 export type TransactionType = 'income' | 'expense'
 
-export type TransactionBlueprint = {
+export interface TransactionBlueprint {
     amount: number
+    exchangeRate?: number
+    currency?: string
 }
 
 export class Transaction implements TransactionBlueprint {
-    constructor(public amount: number) {}
+    amount: number
+    exchangeRate: number = 1
+    currency: string = 'EUR'
+
+    constructor(data: TransactionBlueprint) {
+        this.amount = data.amount
+        this.exchangeRate = data.exchangeRate ? data.exchangeRate : 1
+        this.currency = data.currency ? data.currency : 'EUR'
+    }
 
     type = (): TransactionType => {
         return this.amount > 0 ? 'income' : 'expense'
+    }
+
+    eurEquivalent = (): number => {
+        return this.amount * this.exchangeRate
     }
 }
