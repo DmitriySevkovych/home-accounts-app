e05baaac8c90b144ddb4cda56fac2007915a18cd
ivannaSevkovychItk <ivanna.sevkovych@itk-engineering.de>
2023-04-03 20:02:08 +0200

    Feat: add date and bank accounts to transaction model

--- domain/transactions.ts
+++ domain/transactions.ts
@@ -1,20 +1,29 @@
 export type TransactionType = 'income' | 'expense'
 
 export interface TransactionBlueprint {
+    date?: Date
     amount: number
     exchangeRate?: number
     currency?: string
+    sourceBankAccount?: string | undefined
+    targetBankAccount?: string | undefined
 }
 
 export class Transaction implements TransactionBlueprint {
     amount: number
     exchangeRate: number = 1
     currency: string = 'EUR'
+    date: Date = new Date()
+    sourceBankAccount?: string | undefined
+    targetBankAccount?: string | undefined
 
     constructor(data: TransactionBlueprint) {
         this.amount = data.amount
         this.exchangeRate = data.exchangeRate ? data.exchangeRate : 1
         this.currency = data.currency ? data.currency : 'EUR'
+        this.date = data.date ? data.date : new Date()
+        this.sourceBankAccount = data.sourceBankAccount
+        this.targetBankAccount = data.targetBankAccount
     }
 
     type = (): TransactionType => {
