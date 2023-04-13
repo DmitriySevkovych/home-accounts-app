3008dc3f466072a2bf491c83c76877823ad01e43
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2023-04-13 18:40:24 +0200

    Feat #8: add api routes for getting utils data

---
+++ src/pages/api/v1/utils/transactionCategories.ts
@@ -0,0 +1,29 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getHttpLogger } from '../../../../logging/log-util'
+
+//TODO: #8: load data from a DB
+const temporarilyHardcodedData = [
+    'FOOD',
+    'HOUSEHOLD',
+    'TRANSPORTATION',
+    'BEAUTY',
+    'LEISURE',
+    'VACATION',
+]
+
+const handler = (req: NextApiRequest, res: NextApiResponse): void => {
+    const httpLogger = getHttpLogger('api')
+    httpLogger(req, res)
+
+    switch (req.method) {
+        case 'GET':
+            res.status(200).json(temporarilyHardcodedData)
+            break
+        default:
+            res.status(400)
+            break
+    }
+}
+
+export default handler
