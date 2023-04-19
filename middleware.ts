39fd74d4fa76ed01b9766e178cf92b4e3f2a6c73
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2023-04-19 06:23:34 +0200

    Experiment: try fixing reflect-metadata error for Edge Runtime

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,26 @@
+import { NextRequest, NextResponse } from 'next/server'
 import 'reflect-metadata'
+
+export function middleware(request: NextRequest) {
+    // Clone the request headers and set a new header `x-hello-from-middleware1`
+    const requestHeaders = new Headers(request.headers)
+    requestHeaders.set('x-hello-from-middleware1', 'hello')
+
+    // You can also set request headers in NextResponse.rewrite
+    const response = NextResponse.next({
+        request: {
+            // New request headers
+            headers: requestHeaders,
+        },
+    })
+
+    // Set a new response header `x-hello-from-middleware2`
+    response.headers.set('x-hello-from-middleware2', 'hello')
+    return response
+}
+
+export const config = {
+    unstable_allowDynamic: [
+        '/reflect-metadata/Reflect.js', // use a glob to allow anything in the function-bind 3rd party module
+    ],
+}
