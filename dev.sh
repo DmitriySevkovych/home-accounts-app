4a15d7d080393df70734c31ee69b1ca73c1393a3
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2023-04-19 20:26:00 +0200

    DevOps: split development env variables in multiple files

--- dev.sh
+++ dev.sh
@@ -3,9 +3,14 @@
 # Set necessary environment variables
 set -o allexport
 source .env.local
+source .env
 set +o allexport
 
+if [[ -z "${NEXT_PORT}" ]]; then export NEXT_PORT=8080; fi
+
+# Setup database
 docker-compose -f ./db/docker-compose.db.yml up -d
 
 # Run the Next.js dev task and preppy-print the output using pino
-npx next dev -p 8080 | pino-pretty --colorize --translateTime
+npx next dev -p ${NEXT_PORT} | pino-pretty --colorize --translateTime
+
