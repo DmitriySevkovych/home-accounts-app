068fe921c02a113bf6c3d25300b6847ab5c9a77a
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2023-04-20 06:43:58 +0200

    DevOps: extend dev.sh script to perform db container setup and teardown

---
+++ dev.globalTeardown.js
@@ -0,0 +1,9 @@
+/*
+    A NODE.js script for running setup tasks, not a module
+ */
+
+const { removeDatabaseContainer } = require('./db/dbTeardown.js')
+
+removeDatabaseContainer().then((exitCode) => {
+    process.exit(exitCode)
+})
--- dev.sh
+++ dev.sh
@@ -9,8 +9,12 @@ set +o allexport
 if [[ -z "${NEXT_PORT}" ]]; then export NEXT_PORT=8080; fi
 
 # Setup database
-docker-compose -f ./db/docker-compose.db.yml up -d
+node dev.globalSetup.js
 
-# Run the Next.js dev task and preppy-print the output using pino
-npx next dev -p ${NEXT_PORT} | pino-pretty --colorize --translateTime
+if [[ $? -eq 0 ]]; then
+    # Run the Next.js dev task and preppy-print the output using pino
+    npx next dev -p ${NEXT_PORT} | pino-pretty --colorize --translateTime;
+fi
 
+# Teardowm
+node dev.globalTeardown.js
