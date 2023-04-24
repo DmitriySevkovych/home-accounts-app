5302f1896d4b1d767092c752ce98f0b6f317ae70
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2023-04-24 19:25:50 +0200

    DROP-ME or SQUASH-ME: Comment out windows workaround

--- pages/_gracefulShutdown.ts
+++ pages/_gracefulShutdown.ts
@@ -1,4 +1,4 @@
-import * as keypress from 'keypress'
+// import * as keypress from 'keypress'
 
 import { RepositoryLocator } from '../db/repositoryLocator'
 import { LoggerName, getLogger } from '../logging/log-util'
@@ -9,21 +9,21 @@ export const addShutdownSignalHandlers = (loggerName: LoggerName) => {
     const logger = getLogger(loggerName)
 
     // Windows workaround: https://stackoverflow.com/questions/10021373/what-is-the-windows-equivalent-of-process-onsigint-in-node-js
-    if (process.platform === 'win32') {
-        logger.debug('Adding keypress handler for Ctrl-C on Windows')
-        keypress(process.stdin)
-        process.stdin.resume()
-        process.stdin.setRawMode(true)
-        process.stdin.setEncoding('utf8')
-        process.stdin.on('keypress', (char, key) => {
-            if (key && key.ctrl && key.name == 'c') {
-                logger.debug(
-                    'Received a Ctrl-C on Windows. Will emit SIGINT for further processing'
-                )
-                process.emit('SIGINT', 'SIGINT')
-            }
-        })
-    }
+    // if (process.platform === 'win32') {
+    //     logger.debug('Adding keypress handler for Ctrl-C on Windows')
+    //     keypress(process.stdin)
+    //     process.stdin.resume()
+    //     process.stdin.setRawMode(true)
+    //     process.stdin.setEncoding('utf8')
+    //     process.stdin.on('keypress', (char, key) => {
+    //         if (key && key.ctrl && key.name === 'c') {
+    //             logger.debug(
+    //                 'Received a Ctrl-C on Windows. Will emit SIGINT for further processing'
+    //             )
+    //             process.emit('SIGINT', 'SIGINT')
+    //         }
+    //     })
+    // }
 
     signals.forEach((signal) => {
         logger.debug(`Adding signal handler for ${signal}`)
