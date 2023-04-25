cd967524a5674856007b4bb9685383de7649c0a2
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2023-04-25 06:35:43 +0200

    WIP, not working: add signal handlers only once, on server startup (cf links in comments)

--- next.config.js
+++ next.config.js
@@ -1,10 +1,34 @@
 import nextPWA from 'next-pwa'
 import runtimeCaching from 'next-pwa/cache.js'
 
+import { RepositoryLocator } from './src/db/repositoryLocator.js'
+
+const signals = ['SIGTERM', 'SIGINT']
+
 // Build configuration: https://nextjs.org/docs/api-reference/next.config.js/introduction
 const configuration = () => {
     // Run code once on server start: https://github.com/vercel/next.js/discussions/15341
-    // TODO
+    if (process.env.NEXT_MANUAL_SIG_HANDLE) {
+        signals.forEach((signal) => {
+            console.log(`Adding signal handler for ${signal}`)
+            process.on(signal, async () => {
+                console.log(`Received ${signal}`)
+
+                await RepositoryLocator.closeRepository()
+
+                const timeout = Number(process.env.GRACEFUL_SHUTDOWN_TIMEOUT)
+                setTimeout(
+                    (...args) => {
+                        console.warn(...args)
+                        process.exit(1)
+                    },
+                    timeout,
+                    `Signal ${signal} could not be processed correctly after ${timeout} milliseconds. Exiting hard.`
+                )
+                console.log(`Received ${signal}, end`)
+            })
+        })
+    }
 
     // Configuring next-pwa: https://makerkit.dev/blog/tutorials/pwa-nextjs
     const withPWA = nextPWA({
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,14 +2,9 @@
     Information on gracefully shutting down a Next.js server
         - https://nextjs.org/docs/deployment#manual-graceful-shutdowns
         - https://github.com/vercel/next.js/discussions/38735
+        - Run code once on server start: https://github.com/vercel/next.js/discussions/15341
  */
 import { Html, Head, Main, NextScript } from 'next/document'
-
-import { addShutdownSignalHandlers } from './_gracefulShutdown'
-
-if (process.env.NEXT_MANUAL_SIG_HANDLE) {
-    addShutdownSignalHandlers('document')
-}
 
 const Document = () => {
     return (
