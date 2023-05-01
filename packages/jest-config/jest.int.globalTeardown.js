9786616fb0532f1660b5c1079e60b3fee4aed0b3
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2023-05-01 17:48:46 +0200

    Experiment WIP, SQUASH-ME or DROP-ME: make jest work within packages/

--- packages/jest-config/jest.int.globalTeardown.js
+++
@@ -1,6 +0,0 @@
-import { removeDatabaseContainer } from './src/db/dbTeardown.js'
-
-export default async () => {
-    console.log('Global teardown for integration tests')
-    await removeDatabaseContainer()
-}
