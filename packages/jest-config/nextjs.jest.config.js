15382e311c9aa200ca387ba2d1c1f98bd1495558
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2023-05-01 16:47:59 +0200

    Experiment WIP, SQUASH-ME or DROP-ME: trying out turborepo monorepo

---
+++ packages/jest-config/nextjs.jest.config.js
@@ -0,0 +1,24 @@
+/*
+    For details on separating unit and integration tests in Jest, cf:
+    - https://medium.com/coding-stones/separating-unit-and-integration-tests-in-jest-f6dd301f399c
+*/
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+    // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+    dir: './',
+})
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const customJestConfig = {
+    runner: 'groups',
+    // Add more setup options before each test is run
+    setupFilesAfterEnv: ['<rootDir>/nextjs.jest.setup.js'],
+    // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
+    moduleDirectories: ['node_modules', '<rootDir>/'],
+    testEnvironment: 'jest-environment-jsdom',
+}
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(customJestConfig)
