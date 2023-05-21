8adfa0b9b0e6f7cd2a93cd2d254368291739bc33
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2023-05-21 06:08:51 +0200

    Feat #8: add DB schema creation step to setup script

--- apps/backend/setup.sh
+++ apps/backend/setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
  
-# DB container
+# Create DB container (also creates a database)
 docker-compose -f ./src/db/docker-compose.db.yml up -d
 
-# DB migrations
+# Create DB schemas
+dataSource=./src/db/v1/access/data-source.ts
+query=''
+for schema in utils transactions home work investments; do
+    query="${query}CREATE SCHEMA IF NOT EXISTS ${schema};"
+done
+npm run typeorm query "$query" -- -d $dataSource
