0d55bdee80f963628af18645bb2eebbcc2558543
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2023-06-04 19:28:46 +0200

    Feat #37: introduce TransactionDate class for date- and format-related operations

---
+++ packages/domain-model/src/dates.ts
@@ -0,0 +1,35 @@
+import { DateTime } from 'luxon'
+
+/* Resources:
+    - https://moment.github.io/luxon/index.html
+    - https://moment.github.io/luxon/demo/global.html
+ */
+export class TransactionDate {
+    // https://moment.github.io/luxon/index.html#/formatting
+    static format = 'yyyy-MM-dd'
+
+    static today = (): TransactionDate => {
+        return new TransactionDate(DateTime.now())
+    }
+
+    static fromString = (
+        dateString: string,
+        format: string = TransactionDate.format
+    ): TransactionDate => {
+        return new TransactionDate(DateTime.fromFormat(dateString, format))
+    }
+
+    static fromDatabase = (dateString: string): TransactionDate => {
+        return new TransactionDate(DateTime.fromISO(dateString))
+    }
+
+    private datetime: DateTime
+
+    private constructor(datetime: DateTime) {
+        this.datetime = datetime
+    }
+
+    toString = (): string => {
+        return this.datetime.toFormat(TransactionDate.format)
+    }
+}
