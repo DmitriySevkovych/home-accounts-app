a372df0f1809a39e5e8a4ef18c6cbdd4c449fc48
IvannaSevkovych <Ivanna@Sevkovych.com>
2023-09-10 19:47:06 +0200

    style: adjust index page

--- apps/frontend/src/pages/transactions/index.tsx
+++ apps/frontend/src/pages/transactions/index.tsx
@@ -11,14 +11,21 @@ type TransactionsOverviewProps = {
 const TransactionsOverview = ({ environment }: TransactionsOverviewProps) => {
     return (
         <>
-            <div>Posipaki Home Accounts App: Transactions</div>
-            <Button>
-                <Link href={PAGES.transactions.new}>
-                    Create New Transaction
-                </Link>
-            </Button>
-
-            <footer>Running in {environment} environment</footer>
+            <div className="min-h-screen w-full h-full flex flex-col justify-between p-4">
+                <div className="flex flex-col flex-grow justify-center gap-10 items-center">
+                    <h1 className="font-bold text-xl lg:text-2xl text-primary">
+                        Posipaki Home Accounts App: Transactions
+                    </h1>
+                    <Button>
+                        <Link href={PAGES.transactions.new}>
+                            Create New Transaction
+                        </Link>
+                    </Button>
+                </div>
+                <p className="place-self-end">
+                    Running in {environment} environment
+                </p>
+            </div>
         </>
     )
 }
