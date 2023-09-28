bf74a22d5e16f6bdea466c9ed7ad0d2d53eabc13
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2023-09-28 15:11:52 +0200

    refactor: adjust BACKEND_BASE_URL constant

--- apps/frontend/src/helpers/constants.ts
+++ apps/frontend/src/helpers/constants.ts
@@ -1,1 +1,1 @@
-export const BACKEND_BASE_URL = `${process.env['NEXT_PUBLIC_BACKEND_URL']}/${process.env['NEXT_PUBLIC_BACKEND_API_BASE']}`
+export const BACKEND_BASE_URL = process.env['NEXT_PUBLIC_BACKEND_URL']
