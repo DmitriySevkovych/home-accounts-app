4d8a6733ab519aa39703aef2ffad64e1275ec802
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2023-10-21 05:22:27 +0200

    refactor(frontend): use Next 13 props instead of deprecated props for Image; remove redundant div

--- apps/frontend/src/components/Overlay.tsx
+++ apps/frontend/src/components/Overlay.tsx
@@ -4,14 +4,12 @@ import React from 'react'
 const OverlayImage: React.FC = () => {
     return (
         <div className="pointer-events-none fixed left-0 top-0 z-50 h-full w-full opacity-30 mix-blend-overlay">
-            <div className="relative h-full w-full">
-                <Image
-                    src="/images/overlay_accent.png"
-                    alt="overlay"
-                    layout="fill"
-                    objectFit="contain"
-                />
-            </div>
+            <Image
+                src="/images/overlay_accent.png"
+                alt="overlay"
+                fill
+                style={{ objectFit: 'cover' }}
+            />
         </div>
     )
 }
