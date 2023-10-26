96054c1b40042dd30366b3c824fdd08cc5bf8568
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2023-10-26 10:55:32 +0200

    fix(backend): use context query parameter in GET transactions request

--- apps/backend/src/routes/transactions.routes.ts
+++ apps/backend/src/routes/transactions.routes.ts
@@ -2,39 +2,51 @@ import {
     TransactionValidationError,
     deserializeTransaction,
 } from 'domain-model'
-import express, { type Router } from 'express'
+import express, { type Response, type Router } from 'express'
 import stream from 'stream'
 
 import { RepositoryLocator } from '../db/repositoryLocator'
+import { GetTransactionsRequest } from '../definitions/requests'
 import {
     BadQueryParameterInRequestError,
     NoRecordFoundInDatabaseError,
 } from '../helpers/errors'
-import { checkIdIsInteger } from '../helpers/middleware'
-import { getPaginationOptionsFromRequest } from '../helpers/pagination'
+import * as middleware from '../helpers/middleware'
 import upload, { deserializeTransactionReceipt } from '../helpers/upload'
 
 const getRouter = (): Router => {
     const router = express.Router()
     const repository = RepositoryLocator.getRepository()
 
-    router.get('/', async (req, res) => {
-        try {
-            const paginationOptions = getPaginationOptionsFromRequest(req)
-            const transactions =
-                await repository.getTransactions(paginationOptions)
-            return res.status(200).json(transactions)
-        } catch (err) {
-            req.log.error(err)
-            if (err instanceof BadQueryParameterInRequestError) {
-                res.status(400).json({
-                    message: err.message,
-                })
-            } else {
-                res.status(500).json({ message: 'Something went wrong' })
+    router.get(
+        '/',
+        [middleware.transactionContext, middleware.pagination],
+        async (req: GetTransactionsRequest, res: Response) => {
+            try {
+                /* 
+                REM: transactionContext and paginationOptions cannot be undefined! This is ensured by the middleware.
+                However, to be able to extend the Request interface and comply with the expected middleware types, these fields
+                had to be marked as optional in ../definitions/requests.ts.
+             */
+                const { transactionContext, paginationOptions } = req
+
+                const transactions = await repository.getTransactions(
+                    transactionContext!,
+                    paginationOptions!
+                )
+                return res.status(200).json(transactions)
+            } catch (err) {
+                req.log.error(err)
+                if (err instanceof BadQueryParameterInRequestError) {
+                    res.status(400).json({
+                        message: err.message,
+                    })
+                } else {
+                    res.status(500).json({ message: 'Something went wrong' })
+                }
             }
         }
-    })
+    )
 
     router.post('/', upload.single('receipt'), async (req, res) => {
         try {
@@ -63,7 +75,7 @@ const getRouter = (): Router => {
         }
     })
 
-    router.get('/:id', checkIdIsInteger, async (req, res) => {
+    router.get('/:id', middleware.checkIdIsInteger, async (req, res) => {
         const id = parseInt(req.params.id)
         try {
             const transaction = await repository.getTransactionById(id)
@@ -81,52 +93,56 @@ const getRouter = (): Router => {
         }
     })
 
-    router.get('/:id/receipt', checkIdIsInteger, async (req, res) => {
-        const id = parseInt(req.params.id)
-        try {
-            const transaction = await repository.getTransactionById(id)
+    router.get(
+        '/:id/receipt',
+        middleware.checkIdIsInteger,
+        async (req, res) => {
+            const id = parseInt(req.params.id)
+            try {
+                const transaction = await repository.getTransactionById(id)
 
-            if (!transaction.receiptId) {
-                return res.status(404).send({
-                    message: `The transaction with id=${id} does not have an associated receipt.`,
-                })
-            }
+                if (!transaction.receiptId) {
+                    return res.status(404).send({
+                        message: `The transaction with id=${id} does not have an associated receipt.`,
+                    })
+                }
 
-            const receipt = await repository.getTransactionReceipt(
-                transaction.receiptId
-            )
+                const receipt = await repository.getTransactionReceipt(
+                    transaction.receiptId
+                )
 
-            res.status(200)
-            res.set(
-                'Content-disposition',
-                `attachment; filename=${receipt.name}`
-            )
-            res.set('Content-Type', receipt.mimetype)
+                res.status(200)
+                res.set(
+                    'Content-disposition',
+                    `attachment; filename=${receipt.name}`
+                )
+                res.set('Content-Type', receipt.mimetype)
 
-            /**
-             * Pass the in-memory buffer through a readStream so that it can be downloaded as a file,
-             * cf. {@see ../../docs/api.md#sending-files-in-responses}.
-             *
-             * The straightforward approach res.download() does not work (the download-method expects a path to a file on disk).
-             *
-             * Using res.send(receipt.buffer) does technically work and would probably be simpler. Not sure though if there are any gotchas.
-             * I'll leave the stream.PassThrough implementation for now.
-             */
-            const readStream = new stream.PassThrough()
-            readStream.end(receipt.buffer)
-            readStream.pipe(res)
-        } catch (err) {
-            req.log.error(err)
-            if (err instanceof NoRecordFoundInDatabaseError) {
-                res.status(404).json({
-                    message: 'No record found in the database.',
-                    cause: err.message,
-                })
-            } else {
-                res.status(500).json({ message: 'Something went wrong' })
+                /**
+                 * Pass the in-memory buffer through a readStream so that it can be downloaded as a file,
+                 * cf. {@see ../../docs/api.md#sending-files-in-responses}.
+                 *
+                 * The straightforward approach res.download() does not work (the download-method expects a path to a file on disk).
+                 *
+                 * Using res.send(receipt.buffer) does technically work and would probably be simpler. Not sure though if there are any gotchas.
+                 * I'll leave the stream.PassThrough implementation for now.
+                 */
+                const readStream = new stream.PassThrough()
+                readStream.end(receipt.buffer)
+                readStream.pipe(res)
+            } catch (err) {
+                req.log.error(err)
+                if (err instanceof NoRecordFoundInDatabaseError) {
+                    res.status(404).json({
+                        message: 'No record found in the database.',
+                        cause: err.message,
+                    })
+                } else {
+                    res.status(500).json({ message: 'Something went wrong' })
+                }
             }
         }
-    })
+    )
 
     return router
 }
