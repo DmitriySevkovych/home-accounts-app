a569a6d7dbd1bf7487fe5ebc014ca5620f76620e
IvannaSevkovych <Ivanna@Sevkovych.com>
2023-11-05 18:40:02 +0100

    feat: add basic transactions overview

--- apps/frontend/src/pages/transactions/index.tsx
+++ apps/frontend/src/pages/transactions/index.tsx
@@ -1,3 +1,4 @@
+import { HomeAppDate } from 'domain-model'
 import Link from 'next/link'
 import React from 'react'
 
@@ -9,23 +10,108 @@ import {
 } from '../../helpers/constants'
 import { PAGES } from '../../helpers/pages'
 import { Button } from '../../lib/shadcn/Button'
+import { ScrollArea } from '../../lib/shadcn/ScrollArea'
 
 type TransactionsOverviewProps = {
     systemInfo: SystemInfo
 }
 
+const transactionsData = [
+    {
+        type: 'expense',
+        context: 'home',
+        category: 'FOOD',
+        origin: 'Lidl Schorndorf',
+        description: 'Avocado',
+        date: {
+            datetime: '2023-11-05T01:00:00.000+01:00',
+        },
+        amount: -123,
+        currency: 'EUR',
+        exchangeRate: 1,
+        paymentMethod: 'EC',
+        targetBankAccount: 'HOME_ACCOUNT',
+        tags: ['1'],
+        agent: 'home-app-frontend',
+    },
+    {
+        type: 'expense',
+        context: 'home',
+        category: 'FEE',
+        origin: 'Test 2 dadsgagggggggggggggggggggggggggggggggfgvhhhhhhhhhhh sdfffffffffffg',
+        date: {
+            datetime: '2023-11-05T01:00:00.000+01:00',
+        },
+        amount: -456,
+        currency: 'EUR',
+        exchangeRate: 1,
+        paymentMethod: 'EC',
+        targetBankAccount: 'HOME_ACCOUNT',
+        tags: ['BigAmount', 'Test Tag'],
+        agent: 'home-app-frontend',
+        receiptId: 1,
+    },
+    {
+        type: 'income',
+        context: 'home',
+        category: 'SALARY',
+        origin: 'ITK Engineering GmbH',
+        description: 'November',
+        date: {
+            datetime: '2023-11-05T01:00:00.000+01:00',
+        },
+        amount: 3000,
+        currency: 'EUR',
+        exchangeRate: 1,
+        paymentMethod: 'EC',
+        targetBankAccount: 'HOME_ACCOUNT',
+        tags: [],
+        agent: 'home-app-frontend',
+    },
+]
+
 const TransactionsOverview = ({ systemInfo }: TransactionsOverviewProps) => {
     return (
         <>
             <div className="relative flex h-full min-h-screen w-full flex-col justify-between p-4">
-                <div className="flex flex-grow flex-col items-center justify-center gap-10">
-                    <h1 className="text-center text-xl font-bold text-primary lg:text-2xl">
+                <div className="flex flex-grow flex-col items-center justify-between gap-10">
+                    <h1 className="mt-48 text-center text-xl font-bold text-primary lg:text-2xl">
                         Posipaki Home Accounts App: Transactions
                     </h1>
-                    <Button>
-                        <Link href={PAGES.transactions.new}>
-                            Create New Transaction
-                        </Link>
+                    <div className="flex w-full flex-col gap-3">
+                        <h2>List of transactions</h2>
+                        <ScrollArea className="h-[190px]">
+                            {transactionsData.map((transaction) => {
+                                return (
+                                    <>
+                                        <div className="flex w-full flex-col gap-1 rounded-md border border-input bg-background-overlay px-3 py-2 text-sm font-medium text-primary ring-offset-background">
+                                            <div className="flex w-full justify-between">
+                                                <p className="w-[200px] truncate">
+                                                    {transaction.origin}
+                                                </p>
+                                                <p className="block w-1/3 text-right">
+                                                    {`${transaction.amount} ${transaction.currency}`}
+                                                </p>
+                                            </div>
+                                            <div className="flex w-full justify-between">
+                                                <p>
+                                                    {HomeAppDate.fromISO(
+                                                        transaction.date
+                                                            .datetime
+                                                    ).toWords()}
+                                                </p>
+                                                {transaction.receiptId && (
+                                                    <p>📄</p>
+                                                )}
+                                            </div>
+                                        </div>
+                                    </>
+                                )
+                            })}
+                        </ScrollArea>
+                    </div>
+                    <Button className="self-end" variant="secondary">
+                        <Link href={PAGES.transactions.new}>New</Link>
                     </Button>
                 </div>
                 <OverlayImage />
