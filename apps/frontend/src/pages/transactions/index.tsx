60f78325fae738ff517667c4ca2dc8c21f276218
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2023-12-16 14:58:51 +0100

    feat(frontend): add tabs to preview work and investments transactions

--- apps/frontend/src/pages/transactions/index.tsx
+++ apps/frontend/src/pages/transactions/index.tsx
@@ -1,66 +1,26 @@
-import {
-    Transaction,
-    TransactionContext,
-    deserializeTransaction,
-} from 'domain-model'
 import Link from 'next/link'
-import React, { Suspense, useEffect, useState } from 'react'
 
 import OverlayImage from '../../components/Overlay'
 import { SystemInfo, SystemInfoFooter } from '../../components/SystemInfoFooter'
-import { TransactionPreviewCard } from '../../components/TransactionPreviewCard'
+import { TransactionsPreview } from '../../components/TransactionsPreview'
 import { safeFetch } from '../../helpers/requests'
 import { API, PAGES } from '../../helpers/routes'
 import { Button } from '../../lib/shadcn/Button'
-import { ScrollArea } from '../../lib/shadcn/ScrollArea'
 
 type TransactionsPageProps = {
     systemInfo: SystemInfo
 }
 
-const useLatestTransactions = (context: TransactionContext, limit: number) => {
-    const [transactions, setTransactions] = useState<Transaction[]>([])
-    useEffect(() => {
-        const fetchTransactions = async () => {
-            const req = await safeFetch(
-                API.client.transactions.get(context, limit)
-            )
-            const reqData = await req.json()
-            const fetchedTransactions = reqData.map((obj: any) =>
-                deserializeTransaction(obj)
-            )
-            setTransactions(fetchedTransactions)
-        }
-        fetchTransactions()
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [context, limit])
-    return transactions
-}
-
 const TransactionsPage = ({ systemInfo }: TransactionsPageProps) => {
-    const context = 'home'
-    const limit = 25
-
-    const transactions = useLatestTransactions(context, limit)
-
     return (
         <>
             <div className="relative flex h-full w-full flex-col justify-between p-4">
                 <div className="flex flex-grow flex-col items-center justify-between gap-10">
                     <h1 className="mt-24 text-center text-xl font-bold text-primary lg:text-2xl">
-                        Posipaki Home Accounts App: Transactions
+                        Transactions
                     </h1>
                     <div className="flex w-full flex-col gap-1">
-                        <h1 className="text-md font-bold text-primary lg:text-xl">
-                            Latest
-                        </h1>
-                        <Suspense fallback={<p>Loading...</p>}>
-                            <ScrollArea className="h-[190px]">
-                                {transactions.map((t) => (
-                                    <TransactionPreviewCard key={t.id} {...t} />
-                                ))}
-                            </ScrollArea>
-                        </Suspense>
+                        <TransactionsPreview />
                     </div>
 
                     <Link className="w-full" href={PAGES.transactions.new}>
