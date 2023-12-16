11290655daa4c142ce633407c1122b30acea06b4
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2023-12-16 11:08:28 +0100

    refactor(frontend): extract transaction overview to TransactionOverviewCard

--- apps/frontend/src/pages/transactions/index.tsx
+++ apps/frontend/src/pages/transactions/index.tsx
@@ -2,13 +2,13 @@ import {
     Transaction,
     TransactionContext,
     deserializeTransaction,
-    formatDate,
 } from 'domain-model'
 import Link from 'next/link'
 import React, { Suspense, useEffect, useState } from 'react'
 
 import OverlayImage from '../../components/Overlay'
 import { SystemInfo, SystemInfoFooter } from '../../components/SystemInfoFooter'
+import { TransactionOverviewCard } from '../../components/TransactionOverviewCard'
 import { safeFetch } from '../../helpers/requests'
 import { API, PAGES } from '../../helpers/routes'
 import { Button } from '../../lib/shadcn/Button'
@@ -56,51 +56,12 @@ const TransactionsOverview = ({ systemInfo }: TransactionsOverviewProps) => {
                         </h1>
                         <Suspense fallback={<p>Loading...</p>}>
                             <ScrollArea className="h-[190px]">
-                                {transactions.map((transaction) => {
-                                    const {
-                                        id,
-                                        date,
-                                        category,
-                                        origin,
-                                        amount,
-                                        currency,
-                                        receiptId,
-                                        taxRelevant,
-                                        tags,
-                                    } = transaction
-                                    return (
-                                        <Link
-                                            href={PAGES.transactions.edit(id!)}
-                                            key={id}
-                                        >
-                                            <div className="flex w-full flex-col gap-1 rounded-md border bg-background-overlay px-3 py-2 text-sm font-medium text-primary">
-                                                <div className="flex w-full justify-between">
-                                                    <p className="w-[200px] truncate">
-                                                        {origin}
-                                                    </p>
-                                                    <p className="block w-1/3 text-right">
-                                                        {`${amount} ${currency}`}
-                                                    </p>
-                                                </div>
-                                                <div className="flex w-full justify-between">
-                                                    <p>
-                                                        {category} on{' '}
-                                                        {formatDate(date)}
-                                                    </p>
-                                                    <p>
-                                                        {taxRelevant() && (
-                                                            <>🤓</>
-                                                        )}
-                                                        {receiptId && <>📄</>}
-                                                        {tags.length > 0 && (
-                                                            <>#️⃣</>
-                                                        )}
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </Link>
-                                    )
-                                })}
+                                {transactions.map((t) => (
+                                    <TransactionOverviewCard
+                                        key={t.id}
+                                        {...t}
+                                    />
+                                ))}
                             </ScrollArea>
                         </Suspense>
                     </div>
