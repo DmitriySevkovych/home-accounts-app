8497ee558f7b53348fe7fe62a67e200778f58c5a
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2024-04-01 21:19:16 +0200

    feat(#114, frontend): add zod validation for search dates

--- apps/frontend/src/pages/transactions/search.tsx
+++ apps/frontend/src/pages/transactions/search.tsx
@@ -22,21 +22,33 @@ type SearchTransactionsPageProps = {
     constants: TransactionFormConstants
 }
 
-const SearchParametersFormSchema = z.object({
-    categories: z.optional(z.string().array()),
-    origin: z.optional(z.string()),
-    description: z.optional(z.string()),
-    dateFrom: z.optional(z.coerce.date()),
-    dateUntil: z.optional(z.coerce.date()),
-    tags: z.optional(z.string().array()),
-    // TODO for future feature iterations
-    // taxCategory: z.optional(z.optional(z.string())),
-    // bankAccount: z.optional(z.optional(z.string())),
-    // paymentMethod: z.optional(z.string()),
-    // currency: z.optional(z.string().toUpperCase().length(3)),
-    // investment: z.optional(z.string()),
-    // invoiceKey: z.optional(z.string()),
-})
+const SearchParametersFormSchema = z
+    .object({
+        categories: z.optional(z.string().array()),
+        origin: z.optional(z.string()),
+        description: z.optional(z.string()),
+        dateFrom: z.optional(z.coerce.date()),
+        dateUntil: z.optional(z.coerce.date()),
+        tags: z.optional(z.string().array()),
+        // TODO for future feature iterations
+        // taxCategory: z.optional(z.optional(z.string())),
+        // bankAccount: z.optional(z.optional(z.string())),
+        // paymentMethod: z.optional(z.string()),
+        // currency: z.optional(z.string().toUpperCase().length(3)),
+        // investment: z.optional(z.string()),
+        // invoiceKey: z.optional(z.string()),
+    })
+    .superRefine((form, ctx) => {
+        const { dateFrom, dateUntil } = form
+        // Dates check
+        if (dateFrom && dateUntil && dateFrom > dateUntil) {
+            ctx.addIssue({
+                path: ['dateUntil'],
+                code: z.ZodIssueCode.custom,
+                message: 'Second date must be after first date',
+            })
+        }
+    })
 
 type SearchParameters = z.infer<typeof SearchParametersFormSchema>
 
