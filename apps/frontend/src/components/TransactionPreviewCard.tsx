f8b0d7a2c0050f8f6a108e41be3e4129212a832a
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2024-04-13 08:41:29 +0200

    fix(frontend): display more warnings on transaction preview card

--- apps/frontend/src/components/TransactionPreviewCard.tsx
+++ apps/frontend/src/components/TransactionPreviewCard.tsx
@@ -8,31 +8,31 @@ type TransactionPreviewCardProps = {
     transaction: Transaction
 }
 
-const _previewAmount = (transaction: Transaction): string => {
-    const { amount, currency, exchangeRateWarning } = transaction
-    if (exchangeRateWarning()) return `${amount} ${currency}⚠️`
+const _warningSign = (transaction: Transaction): string => {
+    const { exchangeRateWarning, cashWarning } = transaction
+    if (exchangeRateWarning() || cashWarning()) return '⚠️'
 
-    return `${amount} ${currency}`
+    return ''
 }
 
 const _previewDetails = (transaction: Transaction): string => {
     const { category, date, agent } = transaction
-    if (agent === 'cronjob_blueprint_update')
-        return `🤖 ${category} on ${formatDate(date)}`
+    if (agent.includes('cron')) return `🤖 ${category} on ${formatDate(date)}`
     return `${category} on ${formatDate(date)}`
 }
 
 export const TransactionPreviewCard: React.FC<TransactionPreviewCardProps> = ({
     transaction,
 }) => {
-    const { id, origin, receiptId, taxRelevant, tags } = transaction
+    const { id, origin, amount, currency, receiptId, taxRelevant, tags } =
+        transaction
     return (
         <Link href={PAGES.transactions.edit(id!)} key={id}>
             <div className="mb-1 flex w-full flex-col gap-1 rounded-md border bg-background-overlay px-3 py-2 text-sm font-medium text-primary">
                 <div className="flex w-full justify-between">
-                    <p className="w-[200px] truncate">{origin}</p>
+                    <p className="w-[200px] truncate">{`${_warningSign(transaction)} ${origin}`}</p>
                     <p className="block w-1/3 text-right">
-                        {_previewAmount(transaction)}
+                        {`${amount} ${currency}`}
                     </p>
                 </div>
                 <div className="flex w-full justify-between">
