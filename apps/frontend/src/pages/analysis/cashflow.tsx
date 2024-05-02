aea03bc1f4049dd4b8ec76151c8a33e0a8fdeba6
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2024-05-02 18:03:27 +0200

    wip(#123,frontend): cashflow page, data fetching

--- apps/frontend/src/pages/analysis/cashflow.tsx
+++ apps/frontend/src/pages/analysis/cashflow.tsx
@@ -1,10 +1,39 @@
+import { SearchParameters } from 'domain-model'
+import useSWR from 'swr'
+
 import { SectionHeading } from '../../components/Typography'
 import { PageWithBackButton } from '../../components/pages/PageWithBackButton'
-import { PAGES } from '../../helpers/routes'
+import { safeFetch } from '../../helpers/requests'
+import { API, PAGES } from '../../helpers/routes'
+
+const fetcher = (url: string, parameters: SearchParameters) => {
+    return safeFetch(url, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify({ parameters }),
+    }).then((res) => res.json())
+}
 
 const CashflowAnalysisPage: React.FC = () => {
-    // TODO: fetch data - serverside or clientside?
+    // Local state
+    const parameters: SearchParameters = {
+        searchCombination: 'and',
+        dateFrom: new Date('2024-01-01'),
+    }
 
+    // Queried data
+    const { data, error, isLoading } = useSWR(
+        [API.client.transactions.search({ forceFetchAll: true }), parameters],
+        ([url, parameters]) => fetcher(url, parameters)
+    )
+
+    console.log({ data, error, isLoading })
+
+    // Values computed from queried data
+
+    // Render
     return (
         <PageWithBackButton
             heading="Cashflow Analysis"
