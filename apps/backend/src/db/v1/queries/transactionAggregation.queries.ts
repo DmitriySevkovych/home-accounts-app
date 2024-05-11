c8479c3b86035963721ceb8e588b91b21c5fd338
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2024-05-11 11:00:38 +0200

    feat(#123,backend): exclude ZeroSumTransaction tag from aggregation

--- apps/backend/src/db/v1/queries/transactionAggregation.queries.ts
+++ apps/backend/src/db/v1/queries/transactionAggregation.queries.ts
@@ -8,11 +8,21 @@ export const getGroupedByDate = async (
     const query = {
         name: 'select-from-transactions-group-by-date',
         text: `
-        SELECT category, context, sum(amount*exchange_rate) as amount
-        FROM transactions.transactions
-        WHERE date BETWEEN $1 AND $2 
+        SELECT category, context, sum(amount*exchange_rate) AS amount
+        FROM transactions.transactions t
+        LEFT JOIN (
+            SELECT transaction_id, array_agg(tag) AS tag_array
+            FROM transactions.transaction_tags 
+            GROUP BY transaction_id
+        ) tt ON t.id = tt.transaction_id
+        WHERE date BETWEEN $1 AND $2
+        AND (
+            tag_array IS NULL 
+            OR 
+            NOT($3::varchar[] && tag_array)
+        )
         GROUP BY category, context;`,
-        values: [timeRange.from, timeRange.until],
+        values: [timeRange.from, timeRange.until, ['ZeroSumTransaction']],
     }
 
     const queryResult = await connectionPool.query(query)
