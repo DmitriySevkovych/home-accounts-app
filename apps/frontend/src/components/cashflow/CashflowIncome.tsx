3afd3a66667578ca28734f0fd6076ff18ca2fa77
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2024-05-13 22:48:25 +0200

    feat(#123,frontend): group passive income by investments

--- apps/frontend/src/components/cashflow/CashflowIncome.tsx
+++ apps/frontend/src/components/cashflow/CashflowIncome.tsx
@@ -19,14 +19,11 @@ type IncomeItemProps = {
     monthsConsidered: number
 }
 
-export const IncomeItem: React.FC<IncomeItemProps> = ({
-    item,
-    monthsConsidered,
-}) => {
+const IncomeItem: React.FC<IncomeItemProps> = ({ item, monthsConsidered }) => {
     const { category, investment, origin, amount } = item
     return (
         <div className="grid grid-cols-[75fr_25fr]">
-            <div className="capitalize">{`${category.toLowerCase()} ${investment ? investment : origin}`}</div>
+            <div className="capitalize">{`${investment ? investment : origin} ${category.toLowerCase()}`}</div>
             <div className="text-right">{`${Math.round(amount / monthsConsidered)}€`}</div>
         </div>
     )
@@ -49,7 +46,41 @@ const CashflowIncome: React.FC<CashflowIncomeProps> = ({
     const activeIncome = incomes.filter((i) =>
         ['SALARY', 'SALE'].includes(i.category)
     )
-    const passiveIncome = incomes.filter((i) => !activeIncome.includes(i))
+
+    const passiveIncomeGroupedByInvestment = incomes
+        .filter((i) => !activeIncome.includes(i))
+        .reduce(
+            (
+                result: { [_: string]: TransactionAggregate },
+                item: TransactionAggregate,
+                index: number
+            ) => {
+                // If the reduced item has no investment key, use its index instead (no reducing is necessary then)
+                const investment = item.investment || index.toString()
+
+                // If an entry for this investment already exists, update its values...
+                if (result[investment]) {
+                    const { amount, category, origin } = result[investment]
+                    result[investment] = {
+                        ...result[investment],
+                        amount: amount + item.amount,
+                        category: category.includes(item.category)
+                            ? category
+                            : `${category}, ${item.category}`,
+                        origin: `${origin}, ${item.origin}`,
+                    }
+                } else {
+                    // ...otherwise create a new entry for this investment
+                    result[investment] = item
+                }
+                return result
+            },
+            {}
+        )
+
+    const passiveIncome = Object.values(passiveIncomeGroupedByInvestment).sort(
+        (a, b) => b.amount - a.amount
+    )
 
     // Render
     return (
