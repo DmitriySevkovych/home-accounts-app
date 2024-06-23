49a1fcad54c3f877132a693c4a98b214256cdd0d
Dmitriy Sevkovych <d.sevkovych@varsandnumbers.com>
2024-06-23 11:46:53 +0200

    refactor(frontend): fix nextjs animated image warning

--- apps/frontend/src/pages/transactions/congrats/[transactionType].tsx
+++ apps/frontend/src/pages/transactions/congrats/[transactionType].tsx
@@ -17,17 +17,18 @@ const CongratsPage = ({ transactionType }: CongratsPageProps) => {
             ? 'https://media1.giphy.com/media/LS33kDnx38ZxWm0X2k/giphy.gif?cid=ecf05e475dgt0umidy04jd6lcp4we6lu0hy4hbu9u7lve0x8&ep=v1_gifs_search&rid=giphy.gif&ct=g'
             : 'https://media4.giphy.com/media/MFsqcBSoOKPbjtmvWz/giphy.gif?cid=ecf05e47yy0w3demdaj7n0rdaryw0e74d7yytj7d9p5wbbb5&ep=v1_gifs_search&rid=giphy.gif&ct=g'
 
-    const heading =
+    const caption =
         transactionType === 'expense' ? 'Money well spent😎' : 'Чеки растут🤑'
     return (
         <div className="flex h-full w-full flex-col items-center justify-center gap-10">
-            <MainHeading>{heading}</MainHeading>
+            <MainHeading>{caption}</MainHeading>
             <Image
                 className="rounded-full border-4 border-primary-foreground"
                 width="480"
                 height="480"
-                alt="Money well spent!"
+                alt={caption}
                 src={gifURL}
+                unoptimized
             />
             <Button type="button" size={'lg'}>
                 <Link href={PAGES.transactions.index}>Go back</Link>
